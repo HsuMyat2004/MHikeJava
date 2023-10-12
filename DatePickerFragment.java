2c7c29a8f0f9377eb7e9d1e58eea5d0ab6dfe282
Hsu Myat Htike <147356979+HsuMyat2004@users.noreply.github.com>
2023-10-12 09:08:07 +0630

    Update DatePickerFragment.java

--- DatePickerFragment.java
+++ DatePickerFragment.java
@@ -1,6 +1,5 @@
 package com.kmd.uog.mhike;
 
-
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
@@ -12,19 +11,26 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        LocalDate currentDate = LocalDate.now();
+        int year = currentDate.getYear();
+        int month = currentDate.getMonthValue() - 1; // Subtract 1 to match DatePicker's zero-based month
+        int day = currentDate.getDayOfMonth();
 
-        LocalDate d = LocalDate.now();
-        int year = d.getYear();
-        int month = d.getMonthValue();
-        int day = d.getDayOfMonth();
-        return new DatePickerDialog(getActivity(), this, year, --month, day);
+        return new DatePickerDialog(getActivity(), this, year, month, day);
     }
-
 
     @Override
     public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-        HikeEntryActivity mainActivity =(HikeEntryActivity)getActivity();
-        mainActivity.setDate(LocalDate.of(year, month, day));
-    }
+        // Increment month by 1 to match standard month representation
+        month++;
 
+        if (getActivity() instanceof HikeEntryActivity) {
+            HikeEntryActivity mainActivity = (HikeEntryActivity) getActivity();
+            mainActivity.setDate(LocalDate.of(year, month, day));
+        } else if (getActivity() instanceof HikeAdvancedSearchActivity) {
+            HikeAdvancedSearchActivity searchActivity = (HikeAdvancedSearchActivity) getActivity();
+            searchActivity.setDate(LocalDate.of(year, month, day));
+        }
+    }
 }
+
